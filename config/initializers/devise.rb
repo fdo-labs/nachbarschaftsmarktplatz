3f5dfcec71fda77b7cf66d45be5c493a46993bc1
hesslau <henrik@hesslau.org>
2020-04-03 18:13:16 +0200

    move hardcoded email addresses to secrets.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "kundenservice@fairmondo.de"
+  config.mailer_sender = Rails.application.secrets.default_sender
 
   # Configure the class responsible to send e-mails.
   config.mailer = "DeviseMailer"
