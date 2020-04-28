7ba52e9e2bec77139b33df75c987210b79739e93
hesslau <henrik@hesslau.org>
2020-04-28 12:37:39 +0200

    show map with real data on welcome view

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -71,29 +71,22 @@ module WelcomeHelper
   end
 
   def user_addresses_as_json
-    require 'net/http'
-    require 'uri'
-    require 'json'
-    
-    uri = URI('https://map.nachbarschaftsmarktplatz.de/get_shop_info.php')
-    response = Net::HTTP.get_response(uri)
-    data = JSON.parse(response.body)
-    return data
+    LegalEntity.all.map{ |entity, index| 
+      address = Address.find_by(user:entity); 
 
-    #LegalEntity.all.map{ |entity, index| 
-    #  address = Address.find_by(user:entity); 
-
-    #  puts address
-
-    #  address === nil ? nil : {
-    #      company_name: [address.company_name, entity.fullname].find(&:present?), 
-    #      address: entity.address, 
-    #      about: entity.about_me,
-    #      image: entity.image_url(:profile),
-    #      latitude: 52.489432 + rand(10) * 0.003,
-    #      longitude: 13.386894 + rand(10) * 0.003,
-    #    } 
-
-     # }.reject(&:nil?).to_json
+      if address === nil || address.longitude == nil || address.latitude == nil 
+        nil
+      else 
+        {
+          company_name: [address.company_name, entity.fullname].find(&:present?),  # select either company_name or fullname, wichever is present
+          address: entity.address, 
+          about: entity.about_me,
+          image: entity.image_url(:profile),
+          latitude: address.latitude,
+          longitude: address.longitude,
+          shop_url: user_path(entity)
+        }
+      end
+      }.reject(&:nil?).to_json
   end
 end
